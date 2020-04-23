404e98dcb699717c387c0dc0a5c44b629aec063e
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2020-04-23 15:25:13 +0200

    [ADD] generate_key + elfi_find_section + read_bin + save

--- PoCrypt/include/PoCrypt.h
+++ PoCrypt/include/PoCrypt.h
@@ -21,7 +21,7 @@
 
 /* Basic string obfuscation */
 #define XL(a, i) (char)(a ^ (0x1A4 * 0x2B5 * (i + 1) / 0x45)) /* XOR letter w/ index + keys */
-#define STRING_CIPHER(str) {char *ptr = str ; for (int i = 0; *ptr; ptr++, i++) {*ptr = XL(*ptr, i);}}
+#define XOR_STR(str) {char *ptr = str ; for (int i = 0; *ptr; ptr++, i++) {*ptr = XL(*ptr, i);}}
 
 /* Crypt ".PoC_", "key" */
 #define POC XL('.', 0), XL('P', 1), XL('o', 2), XL('C', 3), XL('_', 4)
@@ -30,6 +30,13 @@
 /* Attribute to move function bin content into CRYPTED_SECTION */
 #define CRYPTED_SECTION ".unix"
 #define CRYPTED __attribute__((section(CRYPTED_SECTION)))
+
+/* Error handling defines */
+#define DIE(s)  {perror(s);exit(1);}
+#define DIE1(s) {fprintf(stderr, s);exit(1);}
+
+/* Bin start in memory here : */
+#define BIN_START ((unsigned char*)0x400000)
 
 /* Crypter */
 typedef struct crypter_s
@@ -43,4 +50,5 @@ typedef struct crypter_s
     int bin_len;
 } crypter_t;
 
+Elf64_Shdr *elfi_find_section (void *data, char *name);
 #endif /* !POCRYPT_H_ */
--- PoCrypt/src/main.c
+++ PoCrypt/src/main.c
@@ -7,13 +7,77 @@
 
 #include "PoCrypt.h"
 
+
+static int get_file_size (int fd)
+{
+  struct stat _infos;
+
+  if (fstat(fd, &_infos) != 0)
+    DIE("fstat");
+  return _infos.st_size;
+}
+
+/* Read binary data and store it */
+crypter_t *read_bin (char *name)
+{
+    int     fd;
+    crypter_t *c;
+
+    if ((c = malloc (sizeof(crypter_t))) == NULL)
+        DIE1("malloc:");
+    c->fname = strdup(name);
+    if ((fd = open (name, O_RDONLY, 0)) < 0)
+        DIE("open");
+    c->bin_len = (get_file_size(fd));
+    c->bin_content = malloc (c->bin_len);
+    read (fd, c->bin_content, c->bin_len);
+    close (fd);
+    return c;
+}
+
+void save(crypter_t *crypt)
+{
+    int fd;
+
+    /* Delete the file so we can write a modified image */
+    if ((unlink (crypt->fname)) < 0)
+        DIE("unlink:");
+    if ((fd = open (crypt->fname, O_CREAT | O_TRUNC | O_RDWR,S_IRWXU)) == -1)
+        DIE("open:");
+    if (write (fd, crypt->bin_content, crypt->bin_len) < 0)
+        DIE("write:");
+    close (fd);
+  return;
+}
+
+char *generate_key (unsigned char *p, int size)
+{
+    char *key = malloc(sizeof(size) + 1);
+    int i;
+
+    if (!key)
+        return NULL;
+    for (i = 0; i < size; i++){
+        p[i] = (rand() % 255);
+        key[i] = p[i];
+    }
+    return key;
+}
+
+/* Malware */
+CRYPTED void payload(void)
+{
+    printf("Gros Malware\n");
+}
+
+/* main */
 int main(int ac, char const * const *av)
 {
     char key_sec[] = {POC, KEY, 0};
+    crypter_t *crypter = read_bin(av[0]);
 
-    STRING_CIPHER(key_sec);
-    printf("%s\n", key_sec);
-    STRING_CIPHER(key_sec);
-    printf("%s\n", key_sec);
+    srand (time(NULL));
+    // decode_and_crypt();
+    payload();
     return (0);
 }
