63305310b950493608430beed8b0d1496c235b19
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2020-07-09 18:16:42 +0200

    meta: dev

---
+++ Metamorphic/Linux-ELF/main.c
@@ -0,0 +1,122 @@
+/*
+** EPITECH PROJECT, 2020
+** Linux-ELF
+** File description:
+** main
+*/
+
+#include "tmp_header.h"
+
+static int get_file_size (int fd)
+{
+    struct stat _infos;
+
+    ASM
+    if (fstat(fd, &_infos) != 0)
+        exit(2);
+    return _infos.st_size;
+}
+
+static unsigned char *read_bin(int fd, int size)
+{
+    unsigned char *res = malloc(sizeof(__u_char) * (size));
+
+    if (res == NULL)
+        exit(1);
+    if (read(fd, res, size) == -1)
+        exit(2);
+        ASM
+    return res;
+}
+
+unsigned char *gen_bit(int bit_nbr);
+
+unsigned char *strstr_asm(unsigned char *bin, unsigned char *pattern, int *remaining) // strstr with non null terminated strings and specific pattern length
+{
+    int c = 0;
+    int i = 0;
+
+    for (; i + 3 < *remaining; i++) {
+        for (; c < PATTERNS_L && pattern[c] == bin[i + c]; c++);
+        if ((c = (c == PATTERNS_L))) {
+            *remaining -= i;
+            return bin + i;
+        }
+    }
+    ASM
+    *remaining -= i;
+    return NULL;
+}
+
+
+static bool valid_pattern(char *pattern) // ugly strcmp
+{
+    for (int i = 0; i < PATTERNS_L; i++)
+        if (pattern[PATTERNS_L + ASM_W_BYTES + i] != PATTERN_E[i])
+            return false;
+    return true;
+}
+
+static void replace_junk(unsigned char **orig, const char *name, int size)
+{
+    unsigned char *pattern = *orig;
+    int remaining = size;
+    unsigned char *rdm_instr;
+    int new_bin_fd = -1;
+    ASM
+
+    for (;remaining >= ASM_TOTAL && (pattern = strstr_asm(pattern, PATTERN_S, &remaining)) != NULL;) {
+
+        // * Is the full pattern recoginzable ?
+        if (!valid_pattern(pattern)) {
+            pattern += PATTERNS_L;
+            remaining -= PATTERNS_L;
+            continue;
+        }
+
+        // * Print
+        printf("\t\t\t\e[96;1m--- VALID PATTERN FOUND ! ---\e[0m\n"\
+               "\e[92;1m- Replacing : \e[0m\n");
+        for (size_t i = 0; i < ASM_W_BYTES; i++)
+              printf("%02x ", pattern[i + PATTERNS_L]);
+        printf("\n\e[92;1m- By : \e[0m\n");
+        ASM
+
+        // * Replace bytes / Sneaky printing
+        rdm_instr = gen_bit(ASM_W_BYTES);
+        for (int i = 0; i < ASM_W_BYTES; i++)
+            pattern[i + PATTERNS_L] = rdm_instr[i];
+        pattern += ASM_TOTAL;
+        remaining -= ASM_TOTAL;
+        DECOY
+        printf("\n");
+    }
+
+    // * Write new file
+    unlink(name);
+    if ((new_bin_fd = open(name, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU)) == -1)
+        exit(2);
+    write(new_bin_fd, *orig, size);
+    close(new_bin_fd);
+}
+
+int main(int argc, char const *argv[])
+{
+    int fd = open(*argv, O_RDONLY);
+    int size = 0;
+    unsigned char *binary = NULL;
+
+    if (fd == -1)
+        exit(2);
+    size = get_file_size(fd); // bin size && Exit on failure
+    binary = read_bin(fd, size); // bin string && Exit on failure
+    ASM
+    close(fd);
+    DECOY
+    srand((intptr_t) *argv); // Randomness
+    srand(time(NULL)/(rand()%10 + 1) * (intptr_t)argv); // Randomness
+    ASM
+    replace_junk(&binary, *argv, size);
+    ASM
+    return 0;
+}
---
+++ Metamorphic/Linux-ELF/tmp_header.h
@@ -0,0 +1,67 @@
+/*
+** EPITECH PROJECT, 2020
+** Linux-ELF
+** File description:
+** tmp_header
+*/
+
+
+#ifndef TMP_HEADER_H_
+#define TMP_HEADER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <keystone/keystone.h>
+
+// ASM opcode pattern :
+#define ASM __asm__(\
+    "pushfq\n"\
+    "pushq rax;\n"\
+    "pushq rbx;\n"\
+    "nop;\n"\
+    "xor rax, rax;\n"\
+    "xor rbx, rax;\n"\
+    "xor rbx, rax;\n"\
+    "nop;nop;nop;\n"\
+    "xor rbx, rbx;\n"\
+    "xor rbx, rax;\n"\
+    "xor rax, rbx;\n"\
+    "nop;\n"\
+    "popq rbx;\n"\
+    "popq rax;\n"\
+    "popfq;");
+
+#define DECOY __asm__("pushfq\n"\
+    "pushq rax;\n"\
+    "pushq rbx;\n" \
+    "popq rbx;\n"\
+    "nop;" \
+    "cmp rax, rax;" \
+    "popq rax;" \
+    "popfq ;\n"\
+    );
+
+// ! This program is made on a x86-64 system !
+#define PATTERN_S "\x9C\x50\x53" // Pattern beggining
+#define PATTERN_E "\x5B\x58\x9D" // Pattern ending
+#define PATTERNS_L 3 // Length E and S patterns
+#define ASM_W_BYTES 23 // how many bytes are free/writable between E and S
+#define ASM_TOTAL 29 // Total bytes ( 23 + 3 * 2 )
+
+int op_cmp_jmp(const char *reg, int bit_nbr, char **code);
+int op_inc_dec(const char *reg, int bit_nbr, char **code);
+int op_nop(const char *reg, int bit_nbr, char **code);
+int op_neg(const char *reg, int bit_nbr, char **code);
+int op_not(const char *reg, int bit_nbr, char **code);
+int op_shl(const char *reg, int bit_nbr, char **code);
+int size_calc(char *code);
+unsigned char * encoder(char *code);
+
+#endif /* !TMP_HEADER_H_ */
