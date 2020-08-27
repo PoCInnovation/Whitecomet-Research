0ec150900a6d6c20f5fd9bbfbb08a1a755704def
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2020-08-27 21:33:16 +0200

    checkout from dev: fix: Fixed base address problem for polymorphism refactor: better polymorphism comments gitignore: .o and PoCrypt

--- PolyMetamorphic/Linux-ELF/src/main.c
+++ PolyMetamorphic/Linux-ELF/src/main.c
@@ -117,15 +117,13 @@ ASM
     key_offset = section->sh_offset;
 
     /* Give W permission on .unix && crypt payload in binary + memory && Remove W permission */
-    change_section_permissions(crypter->seg_offset, crypter->seg_len, true);
-    xor_segment(BIN_START + crypter->seg_offset, crypter->seg_len);
+    change_section_permissions((unsigned char *)&save, crypter->seg_len, true);
+    xor_segment((unsigned char *)&save, crypter->seg_len);
     xor_segment(crypter->bin_content + crypter->seg_offset, crypter->seg_len);
-    change_section_permissions(crypter->seg_offset, crypter->seg_len, false);
+    change_section_permissions((unsigned char *)&save, crypter->seg_len, false);
     ASM
-    /* Give W permission on .PoC_key && rewrite key in binary && Remove W perm on .PoC_key*/
-    change_section_permissions(key_offset, crypter->seg_len, true);
+    /* Rewrite key in binary */
     generate_key(crypter->bin_content + key_offset, crypter->seg_len); // New key
-    change_section_permissions(key_offset, crypter->seg_len, false);
     ASM
     // ! The data's clear here, so we can look for the pattern and change it's instructions
     unsigned char **ptr_bin = (unsigned char **)&(crypter->bin_content);
--- Polymorphic/Linux-ELF/src/ELF/change_sec_perm.c
+++ Polymorphic/Linux-ELF/src/ELF/change_sec_perm.c
@@ -7,11 +7,11 @@
 
 #include "PoCrypt.h"
 
-void change_section_permissions(int seg_offset, int seg_len, bool write)
+/* ptr points to beg. of section */
+void change_section_permissions(unsigned char *ptr, int seg_len, bool write)
 {
-    /* Change Permissions of section  SECTION to decrypt code */
-    unsigned char *ptr = BIN_START + seg_offset;
-    unsigned char *ptr1 = BIN_START + seg_offset + seg_len;
+    /* Change Permissions of section to decrypt code */
+    unsigned char *ptr1 = ptr + seg_len; /* Points to end of section */
     size_t pagesize = sysconf(_SC_PAGESIZE); /* Get system page size */
     uintptr_t pagestart = (uintptr_t)ptr & -pagesize; /* Page of section */
     int psize = (ptr1 - (unsigned char*)pagestart); /* Pagestart size */
@@ -20,8 +20,6 @@ void change_section_permissions(int seg_offset, int seg_len, bool write)
     if (write == true) {
         if (mprotect((void*)pagestart, psize, PROT_READ | PROT_WRITE | PROT_EXEC) < 0)
             DIE("allow_write_on_page");
-    } else {
-        if (mprotect((void*)pagestart, psize, PROT_READ | PROT_EXEC) < 0)
-            DIE("reset_page_perms");
-    }
+    } else if (mprotect((void*)pagestart, psize, PROT_READ | PROT_EXEC) < 0)
+        DIE("reset_page_perms");
 }
